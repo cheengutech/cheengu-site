a86672b95abbdf76f42e46571023779c9c1f1004
Brian Park <hello@cheengu.com>
2026-03-11 22:59:16 -0700

    Changed layout font

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+            <head>
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+              <link 
+                href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@300;400;500&display=swap" 
+                rel="stylesheet" 
+              />
+            </head>
       <body>{children}</body>
     </html>
   )
